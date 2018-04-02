0f12f3534f0207633aa8e2802b34233009433699
yfayman <yfayman@gmail.com>
2018-04-02 08:59:12 -0400

    Added TODO

--- src/main/java/com/example/searcher/UrlSearcher.java
+++ src/main/java/com/example/searcher/UrlSearcher.java
@@ -1,7 +1,7 @@
 package com.example.searcher;
 
 import java.io.IOException;
-
+//TODO seperate out searching and matching. Searching should return a string of all the text in the doc
 public interface UrlSearcher {
 
   public boolean search(String url, String phrase) throws IOException;
